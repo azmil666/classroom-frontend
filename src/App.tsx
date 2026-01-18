bd64e7af77479da20c69386f80bf3629af6ec4aa
Azmil Mohammed <azmilmohammed@Azmils-MacBook-Air.local>
2026-01-18 19:18:55 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {  Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -6,17 +6,23 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import Dashboard from "@/pages/dashboard.tsx";
+import {Home} from "lucide-react";
+import { Layout } from "./components/refine-ui/layout/layout";
+import {BookOpen} from "lucide-react";
+import SubjectsList from "./pages/subjects/list";
+import SubjectsCreate from "./pages/subjects/create";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,11 +35,38 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "QRyxGb-rD8sn9-fETnWe",
               }}
+              resources={[
+                  {
+                      name:'dashboard',
+                      list: '/',
+                      meta: { label: 'Home', icon: <Home />},
+                  },
+                  {
+                      name:'subjects',
+                      list: '/subjects',
+                      create:'/subjects/create',
+                      meta: { label: 'Subjects', icon: <BookOpen />},
+                  }
+              ]}
             >
-              <Routes>
-                <Route index element={<WelcomePage />} />
-              </Routes>
-              <Toaster />
+                <Routes>
+                    <Route
+                        element={
+                            <Layout>
+                                <Outlet />
+                            </Layout>
+                        }
+                    >
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="subjects">
+                            <Route index element={<SubjectsList/>}/>
+                            <Route path="create" element={<SubjectsCreate/>}/>
+
+                        </Route>
+                    </Route>
+                </Routes>
+
+                <Toaster />
               <RefineKbar />
               <UnsavedChangesNotifier />
               <DocumentTitleHandler />
